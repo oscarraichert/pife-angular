22afdbffca1241eaacf08aa752ac7d1ffd0c7569
oscarraichert <raichertoscar@gmail.com>
2025-10-13 22:50:13 -0300

    working chat

--- src/app/game-room/game-room.ts
+++ src/app/game-room/game-room.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { GameService } from '../../services/game.service';
-import { Router } from '@angular/router';
 import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-game-room',
@@ -10,24 +10,37 @@ import { FormControl, ReactiveFormsModule } from "@angular/forms";
   styleUrl: './game-room.css'
 })
 export class GameRoom implements OnDestroy, OnInit {
+  chatMessages: string[] = [];
+  chatMessage = new FormControl('');
+  private destroy$ = new Subject<void>();
 
   constructor(
-    private gameService: GameService
+    private gameService: GameService,
+    private cdr: ChangeDetectorRef,
   ) { }
-  
-  chatMessages: string[] = [];
-  
-  chatMessage = new FormControl();
 
   ngOnInit(): void {
-    this.chatMessages = this.gameService.chatMessages;
+    this.gameService.chatMessages$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: value => {
+          this.chatMessages = value;
+          this.cdr.detectChanges();
+        }
+      });
   }
 
   ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
     this.gameService.leaveRoom();
   }
 
   handleSendMessage() {
-    this.gameService.sendChatMessage(this.chatMessage.value);
+    const msg = this.chatMessage.value?.trim();
+    if (msg) {
+      this.gameService.sendChatMessage(msg);
+      this.chatMessage.reset();
+    }
   }
 }
--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { ApiRoutes } from "../utils/api-routes";
-import { Observable } from "rxjs";
+import { Observable, BehaviorSubject } from "rxjs";
 import { AuthService } from "./auth.service";
 import { WebSocketRoutes } from "../utils/web-socket-routes";
 import { SocketMessage } from "../models/socket-message";
@@ -9,57 +9,69 @@ import { MessageType } from "../models/message-type.enum";
 
 @Injectable({ providedIn: 'root' })
 export class GameService {
-    constructor(
-        private http: HttpClient,
-        private authService: AuthService,
-    ) { }
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+  ) { }
 
-    public socket: WebSocket | null = null;
-    chatMessages: string[] = [];
+  private socket: WebSocket | null = null;
+  private chatMessagesSubject = new BehaviorSubject<string[]>([]);
+  public chatMessages$ = this.chatMessagesSubject.asObservable();
 
-    public connect(url: string): void {
-        this.socket = new WebSocket(url);
+  private messages: string[] = [];
 
-        this.socket.addEventListener("message", (event) => this.handleMessage(JSON.parse(event.data)));
+  public connect(url: string): void {
+    this.socket = new WebSocket(url);
+    
+    this.socket.addEventListener("message", (event) =>
+      this.handleMessage(JSON.parse(event.data))
+    );
+  }
+
+  public disconnect() {
+    this.socket?.close();
+  }
+
+  public send(message: SocketMessage) {
+    this.socket?.send(JSON.stringify(message));
+  }
+
+  private handleMessage(message: SocketMessage) {
+    switch (message.MessageType) {
+      case MessageType.ChatMessage:
+        this.addChatMessage(message.Payload!);
+        break;
+
+      default:
+        console.warn("Unknown message type:", message.MessageType);
     }
+  }
 
-    public disconnect() {
-        this.socket?.close();
-    }
+  private addChatMessage(message: string) {
+    this.messages.push(message);
+    this.chatMessagesSubject.next([...this.messages]); 
+  }
 
-    public send(message: SocketMessage) {
-        this.socket?.send(JSON.stringify(message));
-    }
+  public getRooms(): Observable<string[]> {
+    return this.http.get<string[]>(ApiRoutes.listRooms);
+  }
 
-    private handleMessage(message: SocketMessage) {        
-        switch (message.MessageType) {
-            case MessageType.ChatMessage:
-                this.getChatMessage(message.Payload!);
-        }
-    }    
+  public createRoom() {
+    this.connect(WebSocketRoutes.baseConnection + this.authService.getToken());
+  }
 
-    public getRooms(): Observable<string[]> {
-        return this.http.get<string[]>(ApiRoutes.listRooms);
-    }
+  public joinRoom(roomId: string) {
+    this.connect(WebSocketRoutes.baseConnection + this.authService.getToken() + "&room_id=" + roomId);
+  }
 
-    public createRoom() {
-        this.connect(WebSocketRoutes.baseConnection + this.authService.getToken());
-    }
+  public leaveRoom() {
+    this.disconnect();
+    this.messages = [];
+    this.chatMessagesSubject.next([]);
+  }
 
-    public joinRoom(roomId: string) {
-        this.connect(WebSocketRoutes.baseConnection + this.authService.getToken() + "&room_id=" + roomId);
-    }
-
-    public leaveRoom() {
-        this.disconnect();
-    }
-
-    public sendChatMessage(chatMessage: string) {
-        var message = new SocketMessage(MessageType.ChatMessage, chatMessage);
-        this.send(message);
-    }
-
-    public getChatMessage(message: string) {
-        this.chatMessages.push(message);
-    }
+  public sendChatMessage(chatMessage: string) {
+    const message = new SocketMessage(MessageType.ChatMessage, chatMessage);
+    this.send(message);
+  }
 }
